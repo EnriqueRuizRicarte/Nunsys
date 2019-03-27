7e4409afdc2cd5988db5ea29dd40661d3cac3abf
Enrique Ruiz Ricarte | NUNSYS <enrique.ruizricarte@nunsys.com>
2019-03-27 12:59:07 +0100

    test

---
+++ Nivel02/apiJPA/apiJPA/src/test/java/com/nunsys/apiJPA/ApiJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nunsys.apiJPA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
